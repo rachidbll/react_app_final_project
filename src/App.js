722aefe57f2d3d7c7b8a2cd155763df143105ee2
rachidbll <rachidbll72@gmail.com>
2024-03-09 07:41:39 -0500

    consultation card has been added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './Components/Navbar/Navbar';
 import Layout from './Components/LandngPage/Landing_Page';
 import SignUp from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
+import Consultation from './Components/InstantConsultationBooking/InstantConsultation';
 
 function Home() {
   return <h1>Home Page</h1>;
@@ -26,6 +27,7 @@ function App() {
         <Route path="/" element={<Layout />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/instant-consultation" element={<Consultation />} />
         </Routes>
       </BrowserRouter>
     </div>
